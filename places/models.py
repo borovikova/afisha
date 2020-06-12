527f3878fcd68eb3480b85f27ab71381baff89b2
Ekaterina Borovikova <borovikova@zoho.eu>
2020-06-12 21:03:04 +0200

    Add Place model

--- places/models.py
+++ places/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Place(models.Model):
+    title = models.CharField(max_length=255, verbose_name='название места')
+    placeId = models.CharField(max_length=255, unique=True, verbose_name='уникальный идентификатор')
+    detailsUrl = models.URLField(verbose_name='ссылка на детальные данные')
+    lat = models.FloatField(verbose_name='широта')
+    lon = models.FloatField(verbose_name='долгота')
+
+    def __str__(self):
+        return f"{self.title}"
