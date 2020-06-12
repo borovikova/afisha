4ba000886266465de59eb4534a50bd08f7aff757
Ekaterina Borovikova <borovikova@zoho.eu>
2020-06-12 21:17:47 +0200

    Add Image model

--- places/models.py
+++ places/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Place(models.Model):
     title = models.CharField(max_length=255, verbose_name='название места')
     placeId = models.CharField(max_length=255, unique=True, verbose_name='уникальный идентификатор')
@@ -9,3 +10,13 @@ class Place(models.Model):
 
     def __str__(self):
         return f"{self.title}"
+
+
+class Image(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE,
+                              verbose_name="место, к которому относится картинка", related_name="images")
+    file = models.ImageField(upload_to='images')
+    order = models.PositiveIntegerField(verbose_name="порядок отображения картинки")
+
+    def __str__(self):
+        return f"{self.order} {self.place.title}"
