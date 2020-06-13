376c0090211cadab4ceb96fb75a1064d70af727c
Ekaterina Borovikova <borovikova@zoho.eu>
2020-06-13 14:16:03 +0200

    Get data from db

--- afisha/views.py
+++ afisha/views.py
@@ -1,9 +1,30 @@
 from django.http import HttpResponse
 from django.template import loader
+from places.models import Place
 
 
 def show_map(request):
     template = loader.get_template('index.html')
-    context = {}
+    context = {'data': {
+        'type': 'FeatureCollection',
+        'features': []
+    }
+    }
+    places = Place.objects.all()
+    for place in places:
+        feature = {
+            'type': "Feature",
+            'geometry': {
+                "type": "Point",
+                "coordinates": [place.lon, place.lat]
+            },
+            'properties': {
+                'title': place.title,
+                'placeId': place.id,
+                'detailsUrl': place.detailsUrl
+            }
+        }
+        context['data']['features'].append(feature)
+
     rendered_page = template.render(context, request)
     return HttpResponse(rendered_page)
