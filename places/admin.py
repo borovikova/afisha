3fb71e514d88933f355aa878aa9abef20eba27b3
Ekaterina Borovikova <borovikova@zoho.eu>
2020-06-13 18:02:29 +0200

    Add image preview

--- places/admin.py
+++ places/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Place, Image
+
 
 class ImageInline(admin.TabularInline):
     model = Image
+    readonly_fields = ("preview",)
+
+    def preview(self, obj):
+        return format_html('<img src="{url}" height={height} />',
+                           url=obj.file.url,
+                           # width=obj.headshot.width,
+                           height=200
+                           )
+
 
 class PlaceAdmin(admin.ModelAdmin):
     inlines = [
         ImageInline,
     ]
 
-admin.site.register(Place, PlaceAdmin)
+
 admin.site.register(Image)
+admin.site.register(Place, PlaceAdmin)
