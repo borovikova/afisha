ae765c2b1dde0cbb1b457b2fd2cc6fb51fa39187
Ekaterina Borovikova <borovikova@zoho.eu>
2020-12-17 20:09:14 +0100

    DRY code in Place model

--- places/models.py
+++ places/models.py
@@ -7,12 +7,11 @@ class Place(models.Model):
     title = models.CharField(max_length=255, verbose_name='название места')
     short_description = models.TextField(verbose_name='краткое описание', blank=True)
     long_description = HTMLField(verbose_name='полное описание', blank=True)
-    place_id = models.CharField(max_length=255, unique=True, verbose_name='уникальный идентификатор')
     lat = models.FloatField(verbose_name='широта')
     lon = models.FloatField(verbose_name='долгота')
 
     def __str__(self):
-        return f"{self.title}"
+        return self.title
 
     class Meta(object):
         verbose_name = "локация"
