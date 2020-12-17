418511790972524118eb06647c28882a77b02968
Ekaterina Borovikova <borovikova@zoho.eu>
2020-12-17 20:07:41 +0100

    Include staticfiles urls

--- afisha/urls.py
+++ afisha/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from afisha import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from afisha import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +12,5 @@ urlpatterns = [
     path('places/<int:place_id>/', views.place_detail_view, name='place-detail'),
     path('tinymce/', include('tinymce.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
