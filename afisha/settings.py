db71ea8c08ad03186d1d497b9d05e5d1fdde1d43
Ekaterina Borovikova <borovikova@zoho.eu>
2020-12-17 20:06:57 +0100

    Add static folders

--- afisha/settings.py
+++ afisha/settings.py
@@ -125,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, os.getenv('COLLECTED_STATIC', 'collected_static'))
+STATICFILES_DIRS = [ 
+os.path.join(BASE_DIR, 'assets')
+]
 
 MEDIA_ROOT = 'media/'
 MEDIA_URL = '/media/'
