eeb51afbc83a4a4027186232803874a4383e96e3
Ekaterina Borovikova <borovikova@zoho.eu>
2020-12-17 20:08:53 +0100

    Refactor command

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -1,47 +1,50 @@
-from django.core.management.base import BaseCommand
-from places.models import Place, Image
+import os
 import requests
 import logging
+
+from django.core.management.base import BaseCommand
 from io import BytesIO
-import hashlib
+from urllib.parse import urlparse
+
+from places.models import Place, Image
 
 
 class Command(BaseCommand):
-
-    def get_url(self, url):
-        response = requests.get(url)
-        response.raise_for_status()
-        return response
 
     def add_arguments(self, parser):
         parser.add_argument('url',
                             type=str,
                             help='Link to .json file to parse')
 
-    def save_place(self, data, place_id):
+    def save_place(self, data):
+        lon = data.get('coordinates', {}).get('lng', 0)
+        lat = data.get('coordinates', {}).get('lat', 0)
+        if lon == lat == 0:
+            raise Exception("Coordinates are not valid")
+
         place_data = {
-            'lon': data.get('coordinates', {}).get('lng', 0),
-            'lat': data.get('coordinates', {}).get('lat', 0),
-            'title': data.get('title', ''),
+            'lon': lon,
+            'lat': lat,
             'short_description': data.get('description_short', ''),
             'long_description': data.get('description_long', ''),
-            'place_id': place_id
         }
-        place, created = Place.objects.get_or_create(**place_data)
+        place, created = Place.objects.get_or_create(title=data.get('title', ''), defaults=place_data)
         return place
 
     def save_place_images(self, photos_links, place):
         for link in photos_links:
             img = Image.objects.create(place=place)
-            file_content = self.get_url(link).content
-            hash_ = hashlib.md5(file_content).hexdigest()
-            img.file.save(hash_, BytesIO(file_content))
+            response = requests.get(link)
+            response.raise_for_status()
+            file_content = response.content
+            img.file.save(os.path.basename(urlparse(link).path), BytesIO(file_content))
         return None
 
     def handle(self, *args, **options):
         logging.basicConfig(level=logging.ERROR)
-        raw_data = self.get_url(options['url'])
-        data = raw_data.json()
-        place = self.save_place(data, hashlib.md5(raw_data.content).hexdigest())
+        response = requests.get(options['url'])
+        response.raise_for_status()
+        data = response.json()
+        place = self.save_place(data)
         photos_links = data.get('imgs', [])
         self.save_place_images(photos_links, place)
